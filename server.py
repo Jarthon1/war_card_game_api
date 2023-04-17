b1d5b6e1e602e9c587a40694511999c8efe2de72
Jarthon1 <71906697+Jarthon1@users.noreply.github.com>
2023-04-16 22:16:52 -0400

    Changed war.py to be a class

--- server.py
+++ server.py
@@ -1,4 +1,30 @@
 # This is the File that sets up and manages the RESTful server supporting the card game War.
-import json
-import requests
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+
+games = []  # List to store game information
+
+@app.route('/games', methods=['POST'])  # Define a new endpoint for starting a game with POST method
+def start_game():
+    game_id = len(games) + 1  # Generate a new game ID
+    game = {'id': game_id, 'status': 'started', 'history': []}  # Create a new game object
+    games.append(game)  # Add the game to the games list
+    return jsonify({'message': f'Game {game_id} started!', 'game_id': game_id}), 201  # Return a JSON response with game ID and message
+
+@app.route('/games/<int:game_id>', methods=['GET'])  # Define a new endpoint for getting game history with GET method
+def get_game_history(game_id):
+    game = get_game_by_id(game_id)  # Get the game object by ID
+    if game:
+        return jsonify({'history': game['history']})  # Return the game's history as JSON response
+    else:
+        return jsonify({'error': 'Game not found'}), 404  # Return an error message if game not found
+
+def get_game_by_id(game_id):
+    for game in games:  # Iterate through games list to find game with matching ID
+        if game['id'] == game_id:
+            return game  # Return the game object if found
+    return None  # Return None if game not found
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Start the Flask app in debug mode for development
--- war.py
+++ war.py
@@ -5,12 +5,6 @@ import numpy as np
 import itertools
 import time
 
-# GLOBAL VARIABLES
-
-# GAMESTATE:
-game_over = False
-watch_mode = False
-
 # Suit, (Hex Letter Ranking if it is a face card), Symbol 
 # e.g: Ace of spades: 'SAA'
 DECK = ['C0','C1','C2','C3','C4','C5','C6','C7','C8','C9','CAJ','CBQ','CCK','CDA',
@@ -18,83 +12,96 @@ DECK = ['C0','C1','C2','C3','C4','C5','C6','C7','C8','C9','CAJ','CBQ','CCK','CDA
 		'H0','H1','H2','H3','H4','H5','H6','H7','H8','H9','HAJ','HBQ','HCK','HDA',
 		'S0','S1','S2','S3','S4','S5','S6','S7','S8','S9','SAJ','SBQ','SCK','SDA']
 
-# Variables to store each players hand
-P1_Deck = []
-P2_Deck = []
-
-# war decks:
-P1_Temp = []
-P2_Temp = []
-
 # GAME LOGIC
-# STEP 1 SETUP:
+class war_card_game:
+	def __init__(self):
+		# STEP 1 SETUP:
+		# GAMESTATE:
+		self.game_over = False
+		self.watch_mode = False
+		self.winner = None
 
-# first make a copy and shuffle the deck
-play_deck = copy(DECK)
+		# war decks:
+		self.P1_Temp = []
+		self.P2_Temp = []
 
-# shuffle the deck
-np.random.shuffle(play_deck)
+		# first make a copy of the deck and shuffle it
+		self.play_deck = copy(DECK)
 
-# each player gets half
-P1_Deck = copy(play_deck[0:26])
-P2_Deck = copy(play_deck[26:52])
+		# shuffle the deck
+		np.random.shuffle(self.play_deck)
 
-# STEP 2 GAMEPLAY
+		# each player gets half
+		self.P1_Deck = copy(self.play_deck[0:26])
+		self.P2_Deck = copy(self.play_deck[26:52])
 
-# Gameplay Loop
-while not game_over:
-	p1_card = P1_Deck.pop(0)
-	p2_card = P2_Deck.pop(0)
+	# STEP 2 GAMEPLAY
+	def play_game(self,player1,player2): # Plays a full game of war
+		# Gameplay Loop
+		while not self.game_over:
+			self.play_turn()
+		return self.winner()
+	
+	def play_turn(self, watch_mode = False): # plays the next turn of war
+		# Take the top card from each deck
+		p1_card = self.P1_Deck.pop(0)
+		p2_card = self.P2_Deck.pop(0)
 
-	# Reset war decks:
-	P1_Temp = []
-	P2_Temp = []
+		# Reset war decks:
+		self.P1_Temp = []
+		self.P2_Temp = []
 
-	# War logic
-	while P1_Deck and P2_Deck and p1_card[1] == p2_card[1]:
-		# game_over condition
-		if len(P1_Deck) < 2 or len(P2_Deck) < 2 and not game_over:
-			game_over = True
-			if (len(P1_Deck) > len(P2_Deck)): 
-				winner = 1 
+		# War logic
+		# Continue playing the turn as long as both players have cards and the visible card is equal
+		while self.P1_Deck and self.P2_Deck and p1_card[1] == p2_card[1]:
+			# check if either player has too few cards to continue and if so assign the winner and end the turn
+			if (len(self.P1_Deck) < 2 or len(self.P2_Deck) < 2) and not self.game_over:
+				self.game_over = True
+				if (len(self.P1_Deck) > len(self.P2_Deck)): 
+					self.winner = 1 
+				else: 
+					self.winner = 2
+				print(f"GAMEOVER !! Player {self.winner} won!\n")
+				return
+			
+			# Otherwise add the current face up cards to the war decks
+			self.P1_Temp.append(p1_card)
+			self.P2_Temp.append(p2_card)
+
+			# And then add another card from the top of your personal deck to the war decks
+			self.P1_Temp.append(self.P1_Deck.pop(0))
+			self.P2_Temp.append(self.P2_Deck.pop(0))
+
+			# Finlly deal a new visible card to continue the war
+			p1_card = self.P1_Deck.pop(0)
+			p2_card = self.P2_Deck.pop(0)
+
+		# Now that the current war has ended, give the winning player all cards from both war decks and the visible cards
+		if p1_card[1] > p2_card[1]:
+			self.P1_Deck += self.P1_Temp
+			self.P1_Deck += self.P2_Temp
+			self.P1_Deck.append(p2_card)
+			self.P1_Deck.append(p1_card)
+		else:
+			self.P2_Deck += self.P2_Temp
+			self.P2_Deck += self.P1_Temp
+			self.P2_Deck.append(p1_card)
+			self.P2_Deck.append(p2_card)
+
+		# This prints out the current game state if watch_mode is true
+		if watch_mode:
+			time.sleep(0.2)
+			print(f"Player 1 has {len(self.P1_Deck)} cards left,\nPlayer 2 has {len(self.P2_Deck)} cards left\n\n")
+
+		# Now check if the game is over, and if so, update the winner accordingly
+		if not self.P1_Deck or not self.P2_Deck and not self.game_over:
+			self.game_over == True
+			if (len(self.P1_Deck) > len(self.P2_Deck)): 
+				self.winner = 1 
 			else: 
-				winner = 2
-			print(f"GAMEOVER !! Player {winner} won!\n")
-			break
-
-		P1_Temp.append(p1_card)
-		P2_Temp.append(p2_card)
-
-		P1_Temp.append(P1_Deck.pop(0))
-		P2_Temp.append(P2_Deck.pop(0))
-
-		p1_card = P1_Deck.pop(0)
-		p2_card = P2_Deck.pop(0)
-
-	if p1_card[1] > p2_card[1]:
-		P1_Deck = P1_Deck + P1_Temp
-		P1_Deck = P1_Deck + P2_Temp
-		P1_Deck.append(p2_card)
-		P1_Deck.append(p1_card)
-	else:
-		P2_Deck = P2_Deck + P2_Temp
-		P2_Deck = P2_Deck + P1_Temp
-		P2_Deck.append(p1_card)
-		P2_Deck.append(p2_card)
-
-	if watch_mode:
-		time.sleep(0.2)
-		print(f"Player 1 has {len(P1_Deck)} cards left,\nPlayer 2 has {len(P2_Deck)} cards left\n\n")
-
-	# game_over condition
-	if not P1_Deck or not P2_Deck and not game_over:
-		game_over == True
-		if (len(P1_Deck) > len(P2_Deck)): 
-			winner = 1 
-		else: 
-			winner = 2
-		print(f"GAMEOVER !! Player {winner} won!\n")
-		break
+				self.winner = 2
+			print(f"GAMEOVER !! Player {self.winner} won!\n")
+		return
 
 
 
